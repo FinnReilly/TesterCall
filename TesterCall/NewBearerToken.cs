3148908f74ef9018f3e7638bcd7c55c78318dcb0
Finn Reilly <finn.reilly@netsells.co.uk>
2020-05-23 00:05:19 +0100

    new-bearerToken

---
+++ TesterCall/NewBearerToken.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Text;
+using TesterCall.Services.Usage.AuthStrategies;
+
+namespace TesterCall
+{
+    [Cmdlet(VerbsCommon.New, "BearerToken")]
+    [OutputType(typeof(BearerCredentials))]
+    public class NewBearerToken : Cmdlet
+    {
+        [Parameter(Mandatory = true,
+                    Position = 0)]
+        public string Token { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            var creds = new BearerCredentials(Token);
+
+            WriteObject(creds);
+        }
+    }
+}
---
+++ TesterCall/Services/Usage/AuthStrategies/BearerCredentials.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TesterCall.Services.Usage.AuthStrategies.Interfaces;
+
+namespace TesterCall.Services.Usage.AuthStrategies
+{
+    public class BearerCredentials : IGetAuthorisationHeaderStrategy
+    {
+        private readonly string _bearerToken;
+
+        public BearerCredentials(string bearerToken)
+        {
+            _bearerToken = bearerToken;
+        }
+
+        public Task<string> GetHeader()
+        {
+            return Task.FromResult($"Bearer {_bearerToken}");
+        }
+    }
+}
